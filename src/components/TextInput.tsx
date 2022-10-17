78fa7c5d4fd4d14d3538d742c6396c81fcc254ac
Rickazuo <rick3209@gmail.com>
2022-10-17 19:28:37 -0300

    initial commit

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react'
+import { InputHTMLAttributes } from 'react'
+
+export interface TextInputRootProps {
+    children: ReactNode
+}
+
+function TextInputRoot (props: TextInputRootProps) {
+    return (
+        <div className="flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+        {props.children}
+        </div>   
+    )
+}
+
+//function TextInputIcon {}
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+function TextInputInput(props: TextInputInputProps) {
+    return (    
+        <input
+        className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+        {...props}
+        > 
+        </input>
+    )
+}
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+
+}
