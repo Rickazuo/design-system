e4e17a52d14e53783c59005ffabd2277acf259a7
Rickazuo <rick3209@gmail.com>
2022-10-18 17:39:39 -0300

    add checkbox and text input

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,13 +1,17 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { TextInput, TextInputInputProps, TextInputRootProps } from "./TextInput";
+import { Envelope } from 'phosphor-react'
+import { TextInput, TextInputRootProps } from "./TextInput";
 
 export default {
     title: 'Components/TextInput',
     component: TextInput.Root,
     args: {
-        children: (
+        children: [
+            <TextInput.Icon>
+                <Envelope />
+            </TextInput.Icon>,
             < TextInput.Input placeholder="Type your e-mail address" />
-        ),
+        ],
     },
     argTypes: {
         children: {
@@ -21,3 +25,8 @@ export default {
 
  export const Default: StoryObj<TextInputRootProps> = {}
  
+ export const WithoutIcon: StoryObj<TextInputRootProps> = {
+    args: {
+        children: <TextInput.Input placeholder="Type your e-mail address" />
+    }
+ }
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,3 +1,4 @@
+import { Slot } from '@radix-ui/react-slot'
 import { ReactNode } from 'react'
 import { InputHTMLAttributes } from 'react'
 
@@ -7,13 +8,27 @@ export interface TextInputRootProps {
 
 function TextInputRoot (props: TextInputRootProps) {
     return (
-        <div className="flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+        <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
         {props.children}
         </div>   
     )
 }
 
-//function TextInputIcon {}
+TextInputRoot.displayName = 'TextInput.Root'
+
+export interface TextInputIconProps {
+    children: ReactNode
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+    return (
+        <Slot className="w-6 h-6 text-gray-400">
+            {props.children}
+        </Slot>
+    )
+}
+
+TextInputIcon.displayName = 'TextInput.Icon'
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
@@ -27,8 +42,10 @@ function TextInputInput(props: TextInputInputProps) {
     )
 }
 
+TextInputInput.displayName = 'TextInput.Input'
+
 export const TextInput = {
     Root: TextInputRoot,
     Input: TextInputInput,
-
+    Icon: TextInputIcon,
 }
